716a7b1abfe95ee22ab20968e8293d5cc534d324
Nimesh Tripathi <117906581+Nimesh-Tripathi@users.noreply.github.com>
2024-04-23 02:41:24 +0000

    Container config done

---
+++ Basic/Variables.js
@@ -0,0 +1,22 @@
+const age = 20;
+
+let firstName = "Nimesh";
+let LAstName = "Tripathi";
+
+var place = "Delhi";
+
+console.log(age);
+console.log(firstName);
+console.log(LAstName);
+console.log(place);
+
+firstName = "ishika";
+LAstName = "Tripathi";
+place = "New delhi";
+
+console.log(firstName);
+console.log(LAstName);
+console.log(place);
+
+
+
